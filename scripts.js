4b75048af32edc28c7dc2e590aa3d3148d877fe0
LinkClink <shekaqwe@gmail.com>
2019-09-18 00:27:53 +0300

    Update code:
    - circle : color change
    - clear code (en)

--- scripts.js
+++ scripts.js
@@ -1,36 +1,52 @@
 
-
 var clickedTime;
 var createdTime;
 var reactionTime;
-//funkcja wprowadzenia kolka w losowym miejscu i czasu
-function makeBox() {
 
+//function: entering circle at random place and random time
+function makeBox()
+{
     var time = Math.random();
     time = time * 2000;
-    setTimeout(function() {
+	
+    setTimeout(function() 
+	{
 
-        document.getElementById("kolko").style.borderRadius = "150px";
+        document.getElementById("circle").style.borderRadius = "150px";
 
-
-        var top = Math.random(); //losowa liczba
+        var top = Math.random(); //random value
         top = top * 500;
-        var left = Math.random(); //losowa liczba
+        var left = Math.random(); //random value
         left = left * 1000;
-        document.getElementById("kolko").style.top = top + "px";  //losowe miejsce
-        document.getElementById("kolko").style.left = left + "px";
-        document.getElementById("kolko").style.display = "block";
+        document.getElementById("circle").style.top = top + "px";  //random place
+        document.getElementById("circle").style.left = left + "px";
+        document.getElementById("circle").style.display = "block";
         createdTime = Date.now();
     }, time);
 }
-//funkcja sprawdzania klikniencza
-document.getElementById("kolko").onclick = function() {
+//function: checking touch
+document.getElementById("circle").onclick = function() 
+{
     clickedTime = Date.now();
-    reactionTime = (clickedTime - createdTime) / 1000; //czas reakcji
+    reactionTime = (clickedTime - createdTime) / 1000; //time reaction 
     document.getElementById("time").innerHTML = reactionTime;
-    this.style.display = "none"; //wyczyscia ekran
+    this.style.display = "none"; //display clear
     makeBox();
 }
 
 makeBox();
+
+//changes circle colors
+document.getElementById("circlebut1").onclick = function() 
+{document.getElementById("circle").style.backgroundColor = "green";}
+
+document.getElementById("circlebut2").onclick = function() 
+{document.getElementById("circle").style.backgroundColor = "white";}
+
+document.getElementById("circlebut3").onclick = function() 
+{document.getElementById("circle").style.backgroundColor = "red";}
+
+
+
+
 
